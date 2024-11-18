f7f9a214352afa5ab4f77fe09b47afff4053b417
Clayton Smith <argilo@gmail.com>
2024-11-18 14:02:35 -0500

    Update flags for FM challenge

--- fm/gen.sh
+++ fm/gen.sh
@@ -8,9 +8,12 @@ convert \
   -gravity Center \
   -pointsize 150 \
   -font 'Ubuntu-Sans-Medium' \
-  label:'Part 2: flag\{c5ea1838b44bc323\}' \
+  label:'Part 4: flag\{c5ea1838b44bc323\}' \
   -rotate 270 \
   -extent x2300 \
   -background Black \
   -extent 256x \
   flag1.png
+
+grcc weird_fm.grc
+./weird_fm.py

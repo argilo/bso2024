b31ab0481b4381d34c6302da510285ff7d080dd1
Clayton Smith <argilo@gmail.com>
2024-11-18 11:42:00 -0500

    Split paint flag into three parts

--- paint/gen.sh
+++ paint/gen.sh
@@ -6,7 +6,38 @@ convert \
     -font 'Ubuntu-Sans-Medium' \
     -pointsize 36 \
     -gravity SouthWest \
-    -draw "text 0,0 'flag{foo_BAR_baz}'" \
+    -draw "text 0,0 'flag{pain'" \
+    -pointsize 18 \
     -gravity SouthEast \
     -draw "text 0,0 'VE3IRR'" \
-    flag.png
+    flag1.png
+
+convert \
+    bsides-ottawa-logo.jpg \
+    -extent 1500x550 \
+    -font 'Ubuntu-Sans-Medium' \
+    -pointsize 36 \
+    -gravity SouthWest \
+    -draw "text 0,0 't_all_th'" \
+    -pointsize 18 \
+    -gravity SouthEast \
+    -draw "text 0,0 'VE3IRR'" \
+    flag2.png
+
+convert \
+    bsides-ottawa-logo.jpg \
+    -extent 1500x550 \
+    -font 'Ubuntu-Sans-Medium' \
+    -pointsize 36 \
+    -gravity SouthWest \
+    -draw "text 0,0 'e_things}'" \
+    -pointsize 18 \
+    -gravity SouthEast \
+    -draw "text 0,0 'VE3IRR'" \
+    flag3.png
+
+grcc paint_tx.grc
+
+./paint_tx.py --infile flag1.png --outfile paint1
+./paint_tx.py --infile flag2.png --outfile paint2
+./paint_tx.py --infile flag3.png --outfile paint3
